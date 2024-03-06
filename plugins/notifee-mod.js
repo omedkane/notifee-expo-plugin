6e93caf65c866d7f17494b14692a7f7e21821fc5
uPanda <48337233+omedkane@users.noreply.github.com>
2024-03-06 09:34:44 -0500

    notifee-mod

---
+++ plugins/notifee-mod.js
@@ -0,0 +1,15 @@
+const { withProjectBuildGradle } = require('expo/config-plugins')
+
+module.exports = function withNotifeeRepo(config) {
+  return withProjectBuildGradle(config, async config => {
+    const contents = config.modResults.contents
+    const replacement = `maven { url 'https://www.jitpack.io' }
+        maven {
+            url "$rootDir/../node_modules/@notifee/react-native/android/libs" 
+        }`
+
+    config.modResults.contents = contents.replace("maven { url 'https://www.jitpack.io' }", replacement)
+
+    return config
+  })
+}
